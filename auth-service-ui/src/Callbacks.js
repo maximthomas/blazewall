23e9235a5c7869ae8e61c808b3edfb53d84d9f42
maximthomas <maxim.thomas@gmail.com>
2020-05-15 10:17:23 +0300

    add callback required property

--- auth-service-ui/src/Callbacks.js
+++ auth-service-ui/src/Callbacks.js
@@ -7,10 +7,10 @@ export class Callbacks extends React.Component {
         
         const callbacks = this.props.callbacks.map(callback =>
             <div key={callback.name + "-container"}>
-                <TextField error={!!callback.error} style={{width:"100%", marginTop: "14px"}} key={callback.name} id={callback.name}
-                    type={callback.type} placeholder={callback.prompt}
-                    onChange={(e) => this.props.updateCallback(e.target.value, callback.name)} 
-                    helperText={callback.error}/>
+                <TextField error={!!callback.error} style={{width:"100%", marginTop: "14px"}} key={callback.name}
+                    id={callback.name} type={callback.type} placeholder={callback.prompt}
+                    onChange={(e) => this.props.updateCallback(e.target.value, callback.name)} helperText={callback.error}
+                           value={callback.value} required={callback.required}/>
             </div>)
         const submitBtn = <div><Button  variant="contained" type="submit" color="default">Proceed</Button></div>
         const form = <form onSubmit={this.props.submitCallbacks}>{callbacks}{submitBtn}</form>
